f643ea7087bc7410d2faaa2b7872382d57d106fb
Ronaldo Trejo <ma.ronaldotrejo@gmail.com>
2023-12-19 21:16:40 -0600

    feat: add utility to respond with common http errors

---
+++ app/utils/throws_http_errors.rb
@@ -0,0 +1,18 @@
+##
+# This module is used to throw http errors from the controllers.
+##
+module ThrowsHttpErrors
+
+##
+# This method throws a 404 error.
+def record_not_found
+  render plain: "404 Not Found", status: 404
+end
+
+##
+# This method throws a 500 error.
+def server_error
+  render plain: "500 Internal Server Error", status: 500
+end
+
+end
